f678924da2cd4f909e60c72eb261c5a22eb2e119
Dilkash Peshimam <dilkashpeshimam@gmail.com>
2023-02-28 07:19:10 -0800

    created route & displaying html file

--- httpserver.js
+++ httpserver.js
@@ -1,10 +1,26 @@
 const http=require('http')
+const fs=require('fs')
 
 const port =process.env.PORT || 4000
+
 const server=http.createServer((req,res)=>{
-  res.statusCode=200
-  res.setHeader('Content-Type','text/html')  
-  res.end('<h1>Hello, Wassup!</h1>')
+    if(req.url=='/'){
+        res.statusCode=200
+        res.setHeader('Content-Type','text/html')  
+        res.end('<h1>Hello, Wassup!</h1>')
+    }else if(req.url=='/page'){
+        res.statusCode=200
+        res.setHeader('Content-Type','text/html')  
+        res.end('<h1>Page!</h1>')
+    }else if(req.url=='/hello'){
+      const data=fs.readFileSync('index.html')
+      res.end(data.toString())
+    }
+        else{
+        res.statusCode=404
+        res.end('<h1>Page not found</h1>')
+    }
+
 })
 
 server.listen(port,()=>{
